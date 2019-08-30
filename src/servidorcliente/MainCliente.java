3bb5a9295f3a290003cf3c11b9a844774ef13e9a
daniel9807 <danielmc9807@hotmail.com>
2019-08-30 17:20:37 -0500

    first commit

---
+++ src/servidorcliente/MainCliente.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidorcliente;
+
+/**
+ *
+ * @author BERNARDO MUÑOS
+ */
+import java.io.IOException;
+
+
+public class MainCliente {
+    
+    public static void main(String[] args) throws IOException
+    {
+        Cliente cli = new Cliente(); //Se crea el cliente
+
+        System.out.println("Iniciando cliente\n");
+        cli.startClient(); //Se inicia el cliente
+    }
+    
+}
